ed76f48263065df855e0f438aa5ecc5246f24a5d
Vishal Khot <vishalkhot01@gmail.com>
2021-07-27 19:55:14 +0530

    Feedback display added

--- src/frontend/FeedbackSlide.js
+++ src/frontend/FeedbackSlide.js
@@ -1,54 +1,54 @@
-import axios from "axios"
-import load from "./img/loading.gif"
-import React, { useEffect, useState } from "react"
-import FeedbackList from "./FeedbackList"
+import axios from "axios";
+import load from "./img/loading.gif";
+import React, { useEffect, useState } from "react";
+import FeedbackList from "./FeedbackList";
 export default function FeedbackSlide() {
-  const [reviews, setReviews] = useState("")
-  useEffect(async () => {
-    await getReviews()
-  }, [])
-  const getReviews = async () => {
-    await axios
-      .get("http://localhost:5500/feedback/get")
-      .then((response) => {
-        setReviews(response.data)
-      })
-      .catch((err) => {
-        console.log(err)
-        alert(err)
-        setReviews({ done: 0, error: 1 })
-      })
-  }
+	const [reviews, setReviews] = useState("");
+	useEffect(async () => {
+		await getReviews();
+	}, []);
+	const getReviews = async () => {
+		await axios
+			.get("https://hotel-atlantis-project.herokuapp.com/feedback/get")
+			.then((response) => {
+				setReviews(response.data);
+			})
+			.catch((err) => {
+				console.log(err);
+				alert(err);
+				setReviews({ done: 0, error: 1 });
+			});
+	};
 
-  return (
-    <div style={{ marginLeft: "7px", marginTop: "25px" }}>
-      Some reviews;
-      <div class='loading' id='loading'>
-        <img class='load' src={load} />
-      </div>
-      <table style={{ marginTop: "20px", fontSize: "15px" }}>
-        <tr style={{ fontWeight: "bold" }}>
-          <td>Name</td>
-          <td>Star</td>
-          <td>Review</td>
-        </tr>
+	return (
+		<div style={{ marginLeft: "7px", marginTop: "25px" }}>
+			Some reviews;
+			<div class="loading" id="loading">
+				<img class="load" src={load} />
+			</div>
+			<table style={{ marginTop: "20px", fontSize: "15px" }}>
+				<tr style={{ fontWeight: "bold" }}>
+					<td>Name</td>
+					<td>Star</td>
+					<td>Review</td>
+				</tr>
 
-        <FeedbackList events={reviews} />
-      </table>
-      <div
-        classname='feedback-legend'
-        style={{ fontSize: "20px", marginTop: "20px" }}
-      >
-        <div className='fullStar' style={{ marginLeft: "8px" }}>
-          🌟 - Full Star{" "}
-        </div>
-        <div
-          className='halfStar'
-          style={{ marginLeft: "10px", marginTop: "5px" }}
-        >
-          ☆ &nbsp;- Half Star
-        </div>
-      </div>
-    </div>
-  )
+				<FeedbackList events={reviews} />
+			</table>
+			<div
+				classname="feedback-legend"
+				style={{ fontSize: "20px", marginTop: "20px" }}
+			>
+				<div className="fullStar" style={{ marginLeft: "8px" }}>
+					🌟 - Full Star{" "}
+				</div>
+				<div
+					className="halfStar"
+					style={{ marginLeft: "10px", marginTop: "5px" }}
+				>
+					☆ &nbsp;- Half Star
+				</div>
+			</div>
+		</div>
+	);
 }
